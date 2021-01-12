8f6e725b08b8e5482ccfbe67917765284bf15f87
escojwd <junior19941703@gmail.com>
2021-01-12 16:57:30 +0400

    loading

--- index.js
+++ index.js
@@ -1,54 +1,54 @@
 const path = anime.path('#path');
 
 const timeline = anime.timeline({
-  easing: 'easeInOutExpo',
-  duration: 1000,
-  complete: () => {
-    anime({
-      targets: '.leaf',
-      rotate: 40,
-      duration: 3000,
-      loop: true,
-      direction: 'alternate',
-      easing: 'easeInOutQuad'
-    });
-    anime({
-      targets: '.petals',
-      scale: 1.05,
-      duration: 6000,
-      loop: true,
-      direction: 'alternate',
-      easing: 'easeInOutQuad'
-    });
-  }
+    easing: 'easeInOutExpo',
+    duration: 1000,
+    complete: () => {
+        anime({
+            targets: '.leaf',
+            rotate: 40,
+            duration: 3000,
+            loop: true,
+            direction: 'alternate',
+            easing: 'easeInOutQuad'
+        });
+        anime({
+            targets: '.petals',
+            scale: 1.05,
+            duration: 6000,
+            loop: true,
+            direction: 'alternate',
+            easing: 'easeInOutQuad'
+        });
+    }
 });
 
 timeline.add({
-  targets: '.stem',
-  scale: [0, 1],
+    targets: '.stem',
+    scale: [0, 1],
 })
 
 timeline.add({
-  targets: '.leaf',
-  rotate: [0, 45],
+    targets: '.leaf',
+    rotate: [0, 45],
 })
 timeline.add({
-  targets: '.petals',
-  scale: [0, 1],
+    targets: '.petals',
+    scale: [0, 1],
 }, '-=1000');
 
 timeline.add({
-  targets: '#bee',
-  opacity: [0, 1],
+    targets: '#bee',
+    opacity: [0, 1],
 }, '-=750');
 
 
 anime({
-  targets: '#bee',
-  translateX: path('x'),
-  translateY: path('y'),
-  rotate: path('angle'),
-  loop: true,
-  duration: 12500,
-  easing: 'linear'
+    targets: '#bee',
+    translateX: path('x'),
+    translateY: path('y'),
+    rotate: path('angle'),
+    loop: true,
+    duration: 8500,
+    easing: 'linear'
 });
